1e80b3c2600aceff74007e9b2ed0200028b412c4
JianAi-2021 <huasongnnku@gmail.com>
2021-04-19 21:07:50 +0800

    修改cms的根用户的密码

--- common/config/config.php
+++ common/config/config.php
@@ -14,7 +14,7 @@ return array(
     'db_host' => '127.0.0.1',       // 服务器地址
     'db_name' => 'yzmcms',		    // 数据库名
     'db_user' => 'root',            // 用户名
-    'db_pwd'  => '123456',       	// 密码
+    'db_pwd'  => '10idccom',       	// 密码
     'db_port' => 3306,              // 端口
     'db_prefix' => 'yzm_',          // 数据库表前缀
     
